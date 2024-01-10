bed37c593ecff486fbb54ad5eb84be7aba652ca6
Sujeet <singhsujeet772@gmail.com>
2024-01-10 23:48:43 +0530

    used localstorage to show closed stte website

--- script.js
+++ script.js
@@ -31,11 +31,21 @@ function lightMode(){
 function switchTheme(event){
     if(event.target.checked){
         document.documentElement.setAttribute('data-theme','dark');
+        localStorage.setItem('theme','dark');
         darkMode();
     }else{
         document.documentElement.setAttribute('data-theme','light');
+        localStorage.setItem('theme','light');
         lightMode();
     }
 }
 
 toggleSwitch.addEventListener('change',switchTheme);
+const currentTheme = localStorage.getItem('theme');
+if(currentTheme){
+    document.documentElement.setAttribute('data-theme', currentTheme);
+    if(currentTheme === 'dark'){
+        toggleSwitch.checked = true;
+        darkMode();
+    }
+}
